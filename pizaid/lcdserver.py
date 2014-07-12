9863e25b02bc293116217b4c698b367f8be0ceb0
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-07-12 11:17:17 +0900

    Add pizaid python library

---
+++ pizaid/lcdserver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:   Makoto Shimazu <makoto.shimaz@gmail.com>
+# URL:      https://amiq11.tumblr.com
+# License:  2-Clause BSD License
+# Created:  2014-07-07
+#
+class LCDServer(object):
+    pass
